14c873ba224e64c026661b16758c9cb60af80059
robert.lee <robert0714@gmail.com>
2021-01-08 18:45:53 +0800

    add show outbound ip

--- finish/src/main/java/hello/HelloController.java
+++ finish/src/main/java/hello/HelloController.java
@@ -1,14 +1,34 @@
 package hello;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-    
-    @RequestMapping("/hello")
-    public String hello() {
-        return "Greetings from Spring Boot!";
-    }
-    
+	 
+
+	@RequestMapping("/hello")
+	public String hello() {
+		String des ="";
+		try {
+			 
+			Socket socket = new Socket();
+			socket.connect(new InetSocketAddress("google.com", 80));
+			des =socket.getLocalAddress()!=null ?socket.getLocalAddress().toString() : "" ;
+			 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "Greetings from Spring Boot! "+des;
+	}
+
 }
